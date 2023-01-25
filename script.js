8d2abadae9b28c7cf03b96143b4023a2090f7de5
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2023-01-25 00:03:58 -0300

    Adicionando modificações finais
    adicionei a descrição nos projetos e arrumei a responsividade

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function Elementos(){
 
 
 
-function AddInfos(link, imagem){
+function AddInfos(link, imagem, descri){
     let infos = Elementos();
     let li = infos[0];
     let a = infos[1];
@@ -18,6 +18,7 @@ function AddInfos(link, imagem){
     a.href = link;
     a.target = "_blank";
     img.src = imagem;
+    img.title = descri;
     a.appendChild(img);
     li.appendChild(a);
 
@@ -31,7 +32,7 @@ function Projetos(tipo) {
     let Links = [];
     let Lista = document.getElementById("lista");
     let Imagens = [];
-    let li = document.createElement("li")
+    let Descricao = [];
 
     if(Lista.children.length != 0) {
         let filhos = document.getElementsByClassName("link")
@@ -44,18 +45,21 @@ function Projetos(tipo) {
     if(tipo == "Html-css") {
         Links = ["https://arthur-zct.github.io/siteV/", "https://arthur-zct.github.io/copia-da-interface-do-whatsapp-web/", "https://arthur-zct.github.io/loginTwtcopia/"];
         Imagens = ["img/html-css_1.png", "img/html-css_2.png", "img/html-css_3.png"];
+        Descricao = ["Um site de vendas simples feito apenas com html e css, também possui uma responsividade simples.", "Uma cópia da interface inicial do whatsApp web.", "Uma cópia da interface de login do twitter web."]
     } else if(tipo == "javascript") {
         Links = ["https://arthur-zct.github.io/ListadTarefas/", "https://arthur-zct.github.io/perguntas/"];
         Imagens = ["img/javascript-1.png", "img/javascript-2.png"];
+        Descricao = ["Uma lista de tarefas feita com a opção de adicionar tarefa, excluir, concluir e editar.", "Um site de perguntas bem simples que diz qual linguagem front-end basica é mais adequada para você."]
     } else if (tipo == "react") {
         Links = ["https://Arthur-zct.github.io/imccalculo"];
         Imagens = ["img/react-1.png"];
+        Descricao = ["Um site com uma calculadora de imc feito com react."]
     }
     
     
 
     for(let x = 0; x < Links.length; x++){
-        let a = AddInfos(Links[x], Imagens[x]) 
+        let a = AddInfos(Links[x], Imagens[x], Descricao[x]) 
         Lista.appendChild(a)
     }
 
